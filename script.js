fe09d194ee387c01d74741f1e1e6fe4a8a0c769a
EDHMKY <edhm01@gmail.com>
2021-09-30 09:20:09 -0400

    still am unsure.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+console.log(7);
+
+
+console.log(5%4);
+console.log(5%2);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+
+console.log("Hawk" + "eye");
+
+
+console.log("Ebony".length + 'Harrington-McLeod'.length);
+
+
+console.log("Avengers: Endgame"[16]);
